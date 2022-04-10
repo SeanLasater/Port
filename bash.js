8b3c76aec2faa17bcbb6b54f1cf4614d8527022f
Sean Lasater <SeanLasater@gmail.com>
2022-04-10 13:39:43 -0700

    return text - cats

--- bash.js
+++ bash.js
@@ -1,8 +1,11 @@
 var bashInput = document.getElementById('bash-text');
-var bashOutput = document.getElementById('bash-output')
+var bashOutput = document.getElementById('bash-output');
+var list = "cats";
 
 document.querySelector('input').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        bashOutput.innerHTML = bashInput.value;
+        bashOutput.innerHTML += '<br>' + '~ ' + bashInput.value;
+        bashInput.value = '';
+        bashOutput.innerHTML += '<br>' + list;
         }
 });
